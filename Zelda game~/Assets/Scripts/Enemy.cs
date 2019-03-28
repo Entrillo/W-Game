a623b77d3f944460731c733c940d23f39530f2ab
Entrillo <34277098+Entrillo@users.noreply.github.com>
2019-03-28 09:53:59 +0100

    W-Game

---
+++ Zelda game~/Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyState
+{
+    idle,
+    walk,
+    attack,
+    stagger
+}
+
+public class Enemy : MonoBehaviour
+{
+    public EnemyState currrentState;
+    public FloatValue maxHealth;
+    public float health;
+    public string enemyName;
+    public int baseAttack;
+    public float moveSpeed;
+
+    private void Awake()
+    {
+        health = maxHealth.initialValue;
+    }
+
+    private void TakeDamage(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void Knock(Rigidbody2D myRigidbody, float knockTime, float damage)
+    {
+        StartCoroutine(KnockCo(myRigidbody, knockTime));
+        TakeDamage(damage);
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D myRigidbody, float knocktime)
+    {
+        if (myRigidbody != null)
+        {
+            yield return new WaitForSeconds(knocktime);
+            myRigidbody.velocity = Vector2.zero;
+            currrentState = EnemyState.idle;
+            myRigidbody.velocity = Vector2.zero;
+        }
+    }
+}
